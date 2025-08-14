a8a542b1dcd33cf970f7016299b4a271b4ffb3e7
manishpatidar33 <manishwarrgyiz@gmail.com>
2025-08-14 17:58:31 +0530

    Fix CSS filename typo

--- src/componets/SingUp.jsx
+++ src/componets/SingUp.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Form, useActionData, useNavigate } from 'react-router-dom'
-import './Singup.css';
+import './SingUp.css';
 
 export default function SingUp() {
     const navigate = useNavigate();
